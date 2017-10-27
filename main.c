426e877b476e1bf7e0c6e08501adeba1aeb69250
Apple <apple@Apples-MacBook-Pro.local>
2017-10-28 01:57:15 +0530

    No include config.h

--- main.c
+++ main.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
-#include "config.h"
+// #include "config.h"
 
 #include "function.H"
+
+char array[] = {1, 2, 3, 4};
+
+class A
+{
+    char *arrayp;
+    size_t size;
+public:
+    A(char*i_p, size_t i_size):arrayp(i_p), size(i_size){}
+
+    void print()
+    {
+        for(size_t i=0; i<size; i++)
+        {
+            printf("%d\n", arrayp[i]);
+        }
+    }
+};
 
 int main(int argc, char* argv[])
 {
 #ifdef PROG1
    hello((char*)"PROG1");
-#else
+#endif
+#ifdef PROG2
    hello((char*)"PROG2");
 #endif
+
+    // A a(&array[0], 4);
+    // a.print();
 
    return 0;
 }
